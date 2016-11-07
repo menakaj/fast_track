45373e70709db47e104a61ce18d58fa0ecae33f1
Menaka Jayawardena <menaka@wso2.com>
2016-11-07 23:33:42 +0530

    AXIS2 example added

---
+++ StockQuoteWS/service/src/samples/quickstart/StockQuoteServiceSkeletonInterface.java
@@ -0,0 +1,20 @@
+/**
+ * StockQuoteServiceSkeletonInterface.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.7.4  Built on : Oct 21, 2016 (10:47:34 BST)
+ */
+package samples.quickstart;
+
+/**
+ *  StockQuoteServiceSkeletonInterface java skeleton interface for the axisService
+ */
+public interface StockQuoteServiceSkeletonInterface {
+    /**
+     * Auto generated method signature
+     *
+     * @param getPrice
+     */
+    public samples.quickstart.xsd.GetPriceResponse getPrice(
+        samples.quickstart.xsd.GetPrice getPrice);
+}
